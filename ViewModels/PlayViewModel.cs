b6532e650c7a716dbd22dda56f7f8210b7edac36
Konrad8181 <128095042+Konrad8181@users.noreply.github.com>
2023-04-29 11:44:55 -0700

    Remove not used code

--- ViewModels/PlayViewModel.cs
+++ ViewModels/PlayViewModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MemoryGame.ViewModels
+﻿namespace MemoryGame.ViewModels
 {
     public class PlayViewModel : ViewModelBase
     {
