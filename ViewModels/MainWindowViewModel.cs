338123695ae67745ea56c1088c8745a5b4ac9e44
Konrad8181 <128095042+Konrad8181@users.noreply.github.com>
2023-04-29 11:30:56 -0700

    Create template MVVM project

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace MemoryGame.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace MemoryGame.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
