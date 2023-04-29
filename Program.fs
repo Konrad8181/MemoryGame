bb9ec9cd14a7c920bf722bfd9da457d7b7e01e69
Konrad8181 <128095042+Konrad8181@users.noreply.github.com>
2023-04-29 11:26:23 -0700

    Create template MVVM project

---
+++ Program.fs
@@ -0,0 +1,20 @@
+﻿namespace MemoryGame
+
+open System
+open Avalonia
+open Avalonia.ReactiveUI
+open MemoryGame
+
+module Program =
+
+    [<CompiledName "BuildAvaloniaApp">] 
+    let buildAvaloniaApp () = 
+        AppBuilder
+            .Configure<App>()
+            .UsePlatformDetect()
+            .LogToTrace(areas = Array.empty)
+            .UseReactiveUI()
+
+    [<EntryPoint; STAThread>]
+    let main argv =
+        buildAvaloniaApp().StartWithClassicDesktopLifetime(argv)
---
+++ ViewModels/ViewModelBase.fs
@@ -0,0 +1,6 @@
+﻿namespace MemoryGame.ViewModels
+
+open ReactiveUI
+
+type ViewModelBase() =
+    inherit ReactiveObject()
