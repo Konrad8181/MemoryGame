4c065b94f624893ef2080574871a6d7fb9e075e1
Konrad8181 <128095042+Konrad8181@users.noreply.github.com>
2023-05-06 00:17:07 -0700

    Scores page & mock

---
+++ Models/Score.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MemoryGame.Models
+{
+    public class Score
+    {
+        public DateTime Date { get; set; }
+
+        public string? PlayerName { get; set; }
+
+        public TimeSpan TimeScore { get; set; }
+    }
+}
--- ViewModels/ScoresViewModel.cs
+++ ViewModels/ScoresViewModel.cs
@@ -1,7 +1,43 @@
-﻿namespace MemoryGame.ViewModels
+﻿using MemoryGame.Models;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace MemoryGame.ViewModels
 {
     public class ScoresViewModel : ViewModelBase
     {
-        public string WelcomeString => "Welcome to Scores VM!";
+        private ObservableCollection<Score> _scoresList;
+        public ObservableCollection<Score> ScoresList
+        {
+            get { return _scoresList; }
+            private set { this.RaiseAndSetIfChanged(ref _scoresList, value); }
+        }
+
+        public ScoresViewModel()
+        {
+            var scoresMock = new List<Score>(){
+                new Score()
+                {
+                    Date = DateTime.Now,
+                    PlayerName="Player1",
+                    TimeScore = new TimeSpan(0,2,0)
+                },
+                new Score()
+                {
+                    Date = DateTime.Now,
+                    PlayerName="Player2",
+                    TimeScore = new TimeSpan(4,2,0)
+                },
+                new Score()
+                {
+                    Date = DateTime.Now,
+                    PlayerName="Player3",
+                    TimeScore = new TimeSpan(0,6,9)
+                }
+            };
+            ScoresList = new ObservableCollection<Score>(scoresMock);
+        }
     }
 }
