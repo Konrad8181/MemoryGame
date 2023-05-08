39295e0a92d2a5fb1777c42f0025a32c747265bb
Konrad8181 <128095042+Konrad8181@users.noreply.github.com>
2023-05-08 12:18:43 -0700

    Creating & drawing cards

--- ViewModels/PlayViewModel.cs
+++ ViewModels/PlayViewModel.cs
@@ -1,7 +1,50 @@
-﻿namespace MemoryGame.ViewModels
+﻿using MemoryGame.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MemoryGame.ViewModels
 {
     public class PlayViewModel : ViewModelBase
     {
-        public string WelcomeString => "Welcome to Play VM!";
+        public const int Rows = 4;
+
+        public const int Columns = 5;
+
+        private List<int> _availableTags;
+
+        public List<Card> Cards { get; }
+
+        public PlayViewModel()
+        {
+            Cards = new List<Card>();
+            GenerateRandomTags();
+            CreateCards();
+        }
+
+        private void GenerateRandomTags()
+        {
+            var list = Enumerable.Range(1, (Columns * Rows) / 2).ToList();
+            list.AddRange(list);
+            _availableTags = list.OrderBy(a => Guid.NewGuid()).ToList();
+        }
+
+        private void CreateCards()
+        {
+            var index = 0;
+            for (var row = 0; row < Rows; row++)
+            {
+                for (var column = 0; column < Columns; column++)
+                {
+                    var card = new Card(row, column, HandleClick, _availableTags[index], string.Empty);
+                    Cards.Add(card);
+                    index++;
+                }
+            }
+        }
+
+        private void HandleClick(int x, int y)
+        {
+        }
     }
 }
--- Views/PlayView.axaml.cs
+++ Views/PlayView.axaml.cs
@@ -1,4 +1,9 @@
 using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Controls.Shapes;
+using MemoryGame.ViewModels;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
 
 namespace MemoryGame.Views
 {
@@ -7,6 +12,38 @@ namespace MemoryGame.Views
         public PlayView()
         {
             InitializeComponent();
+            DrawCards();
+        }
+
+        private void DrawCards()
+        {
+            var marigin = 5;
+            if (App.Current.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                if (desktop.MainWindow.DataContext is MainWindowViewModel mainWindowViewModel)
+                {
+                    if (mainWindowViewModel.CurrentPage is PlayViewModel playViewModel)
+                    {
+                        foreach (var card in playViewModel.Cards)
+                        {
+                            var rec = new Rectangle()
+                            {
+                                HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Stretch,
+                                VerticalAlignment = Avalonia.Layout.VerticalAlignment.Stretch,
+                                Fill = card.Brush,
+                                Stroke = card.Brush,
+                                StrokeThickness = 2,
+                                Margin = new Avalonia.Thickness(marigin)
+                            };
+
+                            rec.PointerPressed += (s, e) => card.HandleClick(s, e);
+                            rec.SetValue(Grid.RowProperty, card.Position.x);
+                            rec.SetValue(Grid.ColumnProperty, card.Position.y);
+                            CardsGird.Children.Add(rec);
+                        }
+                    }
+                }
+            }
         }
     }
 }
